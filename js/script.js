8e8277449b7995e2dbee3233f8fda522bede7216
Exicobt <139032501+Exicobt@users.noreply.github.com>
2023-07-10 18:38:15 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,6 +18,22 @@ function getHasil(player, botOption) {
   }
 }
 
+function putar() {
+  const jeda = document.querySelector(".pilihanBot");
+  const putarBot = ["✊", "✌", "✋"];
+  const start = new Date().getTime();
+  let i = 0;
+
+  setInterval(function () {
+    if (new Date().getTime() - start > 1000) {
+      clearInterval;
+      return;
+    }
+    jeda.innerHTML = putarBot[i];
+    i = (i + 1) % putarBot.length;
+  }, 100);
+}
+
 let skorPlayer = 0;
 let skorBot = 0;
 
@@ -28,24 +44,35 @@ pilihanElements.forEach(function (pilihanElement) {
     const human = this.innerHTML;
     const hasil = getHasil(human, computer);
 
-    const pilihanBotElement = document.querySelector(".pilihanBot");
-    pilihanBotElement.innerHTML = computer;
+    putar();
 
     const hasilElement = document.getElementById("result");
-    hasilElement.innerHTML = hasil;
+    hasilElement.innerHTML = "";
+
+    const notification = document.getElementById("pilPlay");
+    notification.innerHTML ="";
 
     if (hasil === "MENANG!") {
       skorPlayer++;
-      skorBot--;
     } else if (hasil === "KALAH!") {
-      skorPlayer--;
       skorBot++;
     }
 
-    const skorPlayerElement = document.getElementById("skorPlayer");
-    skorPlayerElement.innerHTML = skorPlayer;
+    setTimeout(function () {
+      const skorPlayerElement = document.getElementById("skorPlayer");
+      skorPlayerElement.innerHTML = skorPlayer;
 
-    const skorBotElement = document.getElementById("skorBot");
-    skorBotElement.innerHTML = skorBot;
+      const pilihanBotElement = document.querySelector(".pilihanBot");
+      pilihanBotElement.innerHTML = computer;
+
+      const skorBotElement = document.getElementById("skorBot");
+      skorBotElement.innerHTML = skorBot;
+
+      const hasilElement = document.getElementById("result");
+      hasilElement.innerHTML = hasil;
+
+      const notification = document.getElementById("pilPlay");
+      notification.innerHTML = `Kamu memilih ${human}, Bot memilih ${computer}, Hasil pertandingan: ${hasil}`;
+    }, 1000);
   });
 });
